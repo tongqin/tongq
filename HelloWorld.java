5e3db9b091621c3fcbb0a2196648d8584f6e4c2c
David <tongqin2002@gmail.com>
2012-05-10 10:03:41 +0800

    Test .gitignore function

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 	for(int i=0;i<10;i++){
-	    System.out.println(i+":Hello, Dear!");
+	    System.out.println(i+": Hello, Dear!");
 	}
     }
 
